556ec366e5cc704f6a0b206addd0d1ea48547bac
Abhinav Gorantla <abhinav20016@gmail.com>
2026-02-17 11:55:32 -0700

    include timezone in subtitle of the report

--- helper_services/pdf_helper.py
+++ helper_services/pdf_helper.py
@@ -4,7 +4,7 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image, Paragraph, Spacer
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 
 def generate_pdf(yaml_data, causal_recommendation_results, causal_recommendation_vars, unique_id, run_ids, filters):
     # Create a PDF document
@@ -42,7 +42,10 @@ def generate_pdf(yaml_data, causal_recommendation_results, causal_recommendation
 
     # Text above the table
     title = Paragraph("CausalBench: Causal Explanation Report", title_style)
-    subtitle = Paragraph(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), subtitle_style)
+    subtitle = Paragraph(
+        datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC"),
+        subtitle_style
+    )
     effect = Paragraph(f"<b>Summary:</b> {yaml_data['causal_effects']['summary']}", body_style)
     spacer = Spacer(1, 25)
     
